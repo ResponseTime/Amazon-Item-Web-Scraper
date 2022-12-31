1f7f6f75b93258a4a381ea181a9ba40b6600cdeb
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-31 16:22:03 +0530

    Built a check all function for checking the price drop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
-
+from plyer import notification
 load_dotenv()
 
 
@@ -18,7 +18,7 @@ class Base:
         self.price = self.soup.find_all("span", {"class": "a-price-whole"})
         self.p = []
         for i in self.price:
-            self.p.append(int(i.text.replace(",", "")))
+            self.p.append(int(i.text.replace(",", "_")))
         with open('prices.txt', 'w', encoding='utf-16') as prices:
             for i, j in zip(self.name, self.p):
                 prices.writelines(i.text+"["+str(j)+"rs"+"\n")
@@ -39,6 +39,31 @@ class Base:
                 tag.writelines(i+"["+str(j)+"rs"+"\n")
         os.remove("prices.txt")
 
+    def checkForAll(self):
+        self.response = requests.get(url)
+        self.soup = BeautifulSoup(self.response.text, "html.parser")
+        self.name = self.soup.find_all(
+            "span", {"class": "a-size-medium a-color-base a-text-normal"})
+        if len(self.name) == 0:
+            self.name = self.soup.find_all(
+                "span", {"class": "a-size-base-plus a-color-base a-text-normal"})
+        self.price = self.soup.find_all("span", {"class": "a-price-whole"})
+        self.p = []
+        for i in self.price:
+            self.p.append(int(i.text.replace(",", "_")))
+
+        with open("tagged.txt", "r", encoding="utf-16") as tag:
+            self.items = tag.readlines()
+            for i, j in zip(self.name, self.items):
+                if i == j:
+                    notification.notify(
+                        title="Price Dropped",
+                        message="The price of {} has dropped".format(
+                            i.text),
+                        timeout=5
+                    )
+                    print("Price dropped for {}".format(i.text))
+
 
 if __name__ == "__main__":
     Scraper = Base()
@@ -46,3 +71,4 @@ if __name__ == "__main__":
     url = os.getenv("URL").format(item)
     Scraper.getPrices(url)
     Scraper.tagItem()
+    Scraper.checkForAll()
